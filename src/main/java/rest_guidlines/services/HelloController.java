e3314fc279ef04d56206dbb9eafd63c197626a73
dheerajnetwcloud <dheerajnetwcloud@gmail.com>
2024-12-27 11:30:07 +0530

    code changes to connect to sql database

--- src/main/java/rest_guidlines/services/HelloController.java
+++ src/main/java/rest_guidlines/services/HelloController.java
@@ -5,11 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @RestController
 @RequestMapping("/api")
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
+        test();
         return "Hello, World!";
+    }
+
+    public  void test() {
+        String url = "jdbc:mysql://35.153.17.33:3306/mysql";
+        String user = "dheerajr";
+        String password = "12345678";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/rest_guidlines/services/callendar/controller/EventController.java
+++ src/main/java/rest_guidlines/services/callendar/controller/EventController.java
@@ -16,5 +16,4 @@ public class EventController {
         event.setLocation("zoom://please/join/my/zoom");
         return event;
     }
-
 }
